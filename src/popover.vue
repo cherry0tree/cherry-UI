dba8bb5189a9b600e458812b53183260b7eee8f8
hutao <2320702073@qq.com>
2021-01-01 01:07:00 +0800

    add popover compn

---
+++ src/popover.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="poppover" @click="onClick">
+  <div class="content-wrapper" v-if="visible">
+    <slot name="content"></slot>
+  </div>
+    <slot></slot>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'popover',
+  data(){
+    return {
+      visible: false
+    }
+  },
+  methods: {
+    onClick() {
+      this.visible = !this.visible
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+.poppover{
+  display: inline-block;
+  position: relative;
+  .content-wrapper{
+    position: absolute;
+    bottom: 100%;
+    left: 0;
+    border: 1px solid red;
+    box-shadow: 0 0 3px rgba(0,0,0,0.5);
+  }
+  
+}
+</style>
