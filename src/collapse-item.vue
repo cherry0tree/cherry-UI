82676d547b80728a08774ed1f49e48f5f6bf80fc
hutao <2320702073@qq.com>
2021-01-01 21:57:09 +0800

    完成collapse组件

---
+++ src/collapse-item.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="collapseItem">
+  <div class="title" @click="open=!open">
+    {{title}}
+  </div>
+  <div class="content" v-if="open">
+    <slot></slot>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'collapse-item',
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      open: false
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+$grey:#ddd;
+.collapseItem{
+  .title{
+    display: flex;
+    border: 1px solid $grey;
+    margin: -1px -1px 0 -1px;
+    align-items: center;
+    padding: 0 8px;    
+  }
+  > .content{
+    padding: 8px;
+  }    
+}
+</style>
