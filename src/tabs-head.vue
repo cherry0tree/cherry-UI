09c23e2cd8f5e3a963db124310c249337cd4c47e
hutao <2320702073@qq.com>
2021-01-05 18:35:30 +0800

    修复tab页下划线问题

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -13,11 +13,12 @@
 export default {
   name: 'tabs-head',
   inject:['eventBus'],
-  created() {
+  mounted() {
     this.eventBus.$on('update:selected', (item, vm) => {
+      console.log(vm.$el.getBoundingClientRect())
       let {width,height,top,left} = vm.$el.getBoundingClientRect()
       this.$refs.line.style.width = `${width}px`
-      this.$refs.line.style.left  = `${left}px`
+      this.$refs.line.style.left  = `${vm.$el.offsetLeft}px`
     })
   }
 }
@@ -27,17 +28,18 @@ export default {
 .tabs-head{
   position: relative;
   display: flex;
-  border: 1px solid red;
   height: $tab-height;
   >.actions-wrapper{
-    display: flex;
     margin-left: auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
     padding: 0 1em;
   }
   >.line{
     position: absolute;
     bottom: 0;
-    border-bottom: 4px solid blue;
+    border-bottom: 1px solid blue;
     transition: all 350ms;
   }
   
