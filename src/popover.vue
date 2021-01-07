b664fdad60a598fa9caf7ce6aedd57dca110b5e0
hutao <2320702073@qq.com>
2021-01-07 14:23:47 +0800

    重构popover

--- src/popover.vue
+++ src/popover.vue
@@ -1,12 +1,9 @@
 <template>
-<div class="poppover" ref="popover">
-  <div class="content-wrapper" 
-       :class="{[`position-${position}`]: true}"
-       ref="contentWrapper" 
-       v-if="visible">
+<div class="popover" ref="popover">
+  <div class="contentWrapper" :class="contentClass" v-if="visible">
     <slot name="content"></slot>
   </div>
-  <span ref="triggerWrapper" style="display: inline-block">
+  <span style="display: inline-block">
     <slot></slot>
   </span>
 </div>
@@ -15,12 +12,12 @@
 <script>
 export default {
   name: 'popover',
-  props: {
+  props:{
     position: {
       type: String,
       default: 'top',
       validator(value){
-        return ['top', 'bottom', 'left', 'right'].indexOf(value) >= 0
+        return ['top','right','bottom','left'].indexOf(value) >= 0
       }
     },
     trigger: {
@@ -31,117 +28,122 @@ export default {
       }
     }
   },
-  data(){
+  computed: {
+    contentClass(){
+      return [`position-${this.position}`]
+    }
+  },
+  data() {
     return {
       visible: false
     }
   },
-  mounted(){
+  mounted() {
     if(this.trigger === 'click') {
       this.$refs.popover.addEventListener('click', this.onClick)
     } else {
       this.$refs.popover.addEventListener('mouseenter', this.open)
-      this.$refs.popover.addEventListener('mouseleave', this.close)
+      this.$refs.popover.addEventListener('mouseleave', this.close)      
     }
   },
   methods: {
-    open() {
+    onClick(){
+      this.visible = !this.visible
+    },
+    open(){
       this.visible = true
     },
-    close() {
+    close(){
       this.visible = false
-    },
-    onClick(event) {
-      if(this.$refs.triggerWrapper.contains(event.target)) {
-        if(this.visible === true) {
-          this.close()
-        } else {
-          this.open()
-        }
-      }
     }
   }
 }
 </script>
 <style scoped lang="scss">
 @import "helper";
-.poppover{
+
+.popover {
   display: inline-block;
   position: relative;
-  .content-wrapper{
+  .contentWrapper {
     position: absolute;
-    border: 1px solid $border-color;
+    border: solid 1px $popover-border-color;
     border-radius: $border-radius;
+    filter: drop-shadow(0 1px 1px rgba(0, 0, 0, 0.5));
     background: white;
-    filter: drop-shadow(0 1px 1px rgba(0,0,0,0.5));
     padding: .5em 1em;
+    max-width: 20em;
     &::before, &::after{
       content: '';
       display: block;
+      position: absolute;
       border: solid 10px transparent;
       width: 0;
       height: 0;
-      position: absolute;     
     }
     &.position-top{
       transform: translateY(-100%);
       margin-top: -10px;
-      &::before{
+      &::before, &::after {
+        left: 10px;
+      }      
+      &::before {
         border-top-color: black;
-        top:100%;
+        top: 100%;
       }
-      &::after{
+      &::after {
         border-top-color: white;
         top: calc(100% - 1px);
-      } 
+      }      
     }
-    &.position-bottom {
+    &.position-bottom{
       transform: translateY(100%);
       margin-top: -10px;
-      &::before,&::after{
+      &::before, &::after {
         left: 10px;
       }
-      &::before{
+      &::before {
         border-bottom-color: black;
-        bottom:100%
+        bottom: 100%
       }
-      &::after{
+      &::after {
         border-bottom-color: white;
         bottom: calc(100% - 1px);
-      }    
+      }      
     }
     &.position-left{
       transform: translateX(-100%);
       margin-left: -10px;
-      &::before,&::after{
+      &::before, &::after {
         transform: translateY(-50%);
         top: 50%;
       }
-      &::before{
+      &::before {
         border-left-color: black;
-        left:100%
+        left: 100%
       }
-      &::after{
+      &::after {
         border-left-color: white;
         left: calc(100% - 1px);
-      }    
+      }      
     }
     &.position-right{
       transform: translateX(100%);
       margin-left: -10px;
-      &::before,&::after{
+      &::before, &::after {
         transform: translateY(-50%);
         top: 50%;
       }
-      &::before{
+      &::before {
         border-right-color: black;
-        right:100%
+        right: 100%
       }
-      &::after{
+      &::after {
         border-right-color: white;
         right: calc(100% - 1px);
-      }
-    }
-  }
+      }      
+    }            
+  }  
 }
+
 </style>
